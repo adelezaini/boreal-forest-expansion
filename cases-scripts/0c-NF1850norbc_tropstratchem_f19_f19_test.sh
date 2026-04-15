1f1656eb7d8cb7632e8f06b4974b07c48f2ec427
Adele Zaini <adelez@login-1.betzy.sigma2.no>
2026-04-15 15:36:24 +0200

    Add initial files to the simulation script and fix some bugs

--- cases-scripts/0c-NF1850norbc_tropstratchem_f19_f19_test.sh
+++ cases-scripts/0c-NF1850norbc_tropstratchem_f19_f19_test.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
-# NF18500_tropstratchem test case to check output variables
+# NF18500_tropstratchem test case to test and check output variables
+
+# Exit if error, undefined variable...
+set -euo pipefail
+# Load common functions for case setup
+source cases-setup.sh
 
 # Simulation specifics:
-export CASENAME=NF1850norbc_tropstratchem_f19_f19_test
-export PROJECT=nn9188k
-export NORESM_ROOT=/cluster/home/$USER/NorESM2.3_beta01
+#––––––––––– For tests - adding date to name to avoid overwriting previous tests: 
+today=$(date +'%Y%m%d')
+testname="test-$today"
+export CASENAME="NF1850norbc_tropstratchem_f19_f19_$testname"
 export COMPSET=1850_CAM60%NORESM%NORBC%TROPSTRATCHEM_CLM50%BGC_CICE%PRES_DOCN%DOM_MOSART_SGLC_SWAV
-export RES=f19_f19
+set_project_noresm_res_vars
 
-# Initial files:
-export REFCASE=NF1850norbc_f19_f19_20241127_test01
-export REFDATE=0031-01-01
-export REFDIR=run
+# Restart files specifics:
+# Use some Dirk's restart files
+REFCASE="NHIST_tropstratchem_01_f19_tn14_r1990_s01_20241118"
+REFDATE="2000-01-01"
+
+REST_SRC="/nird/datalake/NS9560K/olivie/restart/${REFCASE}/${REFDATE}-00000"
+REST_LOCAL="/cluster/home/$USER/restart/${REFCASE}_${REFDATE}-00000"
+
 #–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+prepare_restart_files "$REST_SRC" "$REST_LOCAL"
 
 CASEROOT=$HOME/cases/BRL_FRST_XPSN/$CASENAME
-rm -rf $CASEROOT #remove previous cases
+rm -rf "$CASEROOT" #remove previous case
 
 cd $NORESM_ROOT/cime/scripts || exit 1
 
@@ -24,77 +35,30 @@ cd $NORESM_ROOT/cime/scripts || exit 1
 
 cd $CASEROOT
 
-./xmlchange CAM_AEROCOM=TRUE #aerosol diagnostics
+aerosol_cosp_diagnostics
+forcings_2000
 
-#––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
-mkdir -p $CASEROOT/run
-cp /nird/datalake/NS9560K/noresm2.3/cases/$REFCASE/rest/${REFDATE}-00000/* $CASEROOT/run/
-gunzip $CASEROOT/run/*.gz 
-echo "Copied restarts from $REFCASE at $REFDATE to $CASEROOT/run/ and unzipped them"
-
+# Initial files from restart
 ./xmlchange RUN_TYPE=hybrid
-./xmlchange RUN_REFCASE=$REFCASE
-./xmlchange RUN_REFDATE=$REFDATE
-#./xmlchange STOP_OPTION=nyears,STOP_N=1
-# Alternatevely: I could directly copy to RUNDIR=/cluster/projects/nn9188k/adelez/cases/$CASENAME/run and avoid the next commands
-./xmlchange RUN_REFDIR=$REFDIR                     # path to restarts 
-./xmlchange GET_REFCASE=TRUE                       # get refcase from outside rundir
-#––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+./xmlchange RUN_REFCASE="$REFCASE"
+./xmlchange RUN_REFDATE="$REFDATE"
+./xmlchange RUN_REFDIR="$REST_LOCAL"
+./xmlchange GET_REFCASE=TRUE
 
-#./xmlchange --subgroup case.st_archive JOB_WALLCLOCK_TIME=23:59:00
-#./xmlchange --subgroup case.run        JOB_WALLCLOCK_TIME=48:59:00
-
-#./case.build --clean
-./case.setup
-
-# Diagnostics
-cat << EOF >> user_nl_cam
- mfilt = 1, 48
- nhtfrq = 0, 1
- avgflag_pertape='A','I' 
- history_aerosol=.true.
- fincl1 = 'NNAT_0','FSNT','FLNT','FSNT_DRF','FLNT_DRF','FSNTCDRF','FLNTCDRF','FLNS','FSNS','FLNSC','FSNSC',
-'FSDSCDRF','FSDS_DRF','FSUTADRF','FLUTC','FSUS_DRF','FLUS','CLOUD','FCTL','FCTI','NUCLRATE','FORMRATE',
-'GRH2SO4','GRSOA','GR','COAGNUCL','H2SO4','SOA_LV','PS','LANDFRAC','SOA_NA','SO4_NA',
-
-'NCONC01','NCONC02','NCONC03','NCONC04','NCONC05','NCONC06','NCONC07','NCONC08','NCONC09','NCONC10','NCONC11','NCONC12','NCONC13','NCONC14',
-'SIGMA01','SIGMA02','SIGMA03','SIGMA04','SIGMA05','SIGMA06','SIGMA07','SIGMA08','SIGMA09','SIGMA10','SIGMA11','SIGMA12','SIGMA13','SIGMA14',
-'NMR01','NMR02','NMR03','NMR04','NMR05','NMR06','NMR07','NMR08','NMR09','NMR10','NMR11','NMR12','NMR13','NMR14',
-
-
-'CCN1','CCN2','CCN3','CCN4','CCN5','CCN6','CCN7','CCN_B','TGCLDCWP','cb_H2SO4','cb_SOA_LV','cb_SOA_NA','cb_SO4_NA',
-'CLDTOT','CDNUMC','SO2','isoprene','monoterp','SOA_SV','OH_vmr','AOD_VIS','CAODVIS','CLDFREE',
-'CDOD550','CDOD440','CDOD870','AEROD_v','CABS550','CABS550A',
-
-'SST','PRECC','PRECL','PRECT','ozone','O3','TROP_P','TROP_T','TROP_Z','VT100',
-'MEG_isoprene','MEG_monoterp', 'SFisoprene','SFmonoterp','cb_isoprene','cb_monoterp',
-'MEG_CH3COCH3','MEG_CH3CHO','MEG_CH2O','MEG_CO','MEG_C2H6','MEG_C3H8','MEG_C2H4','MEG_C3H6', 'MEG_C2H5OH','MEG_C10H16','MEG_ISOP','MEG_CH3OH'
- fincl2 = 'SFisoprene','SFmonoterp',
-'TROP_O3', 'TROP_NO', 'TROP_NO2', 'TROP_H2O2', 'TROP_HNO3', 'TROP_CO', 'TROP_SO2', 'TROP_NH3', 'TROP_HCHO', 'TROP_CH4', 'TROP_C2H6', 'TROP_C3H8', 'TROP_C2H4', 'TROP_C3H6', 'TROP_C2H5OH', 'TROP_C10H16', 'TROP_ISOP', 'TROP_CH3OH', 'TROP_isoprene', 'TROP_monoterp'
-EOF
-
-# Look when full atmospheric chemistry:
-# fincl1 = 'MEG_CH3COCH3','MEG_CH3CHO','MEG_CH2O','MEG_CO','MEG_C2H6','MEG_C3H8','MEG_C2H4','MEG_C3H6', 'MEG_C2H5OH','MEG_C10H16','MEG_ISOP','MEG_CH3OH'
-# fincl2 = 'TROP_O3', 'TROP_NO', 'TROP_NO2', 'TROP_H2O2', 'TROP_HNO3', 'TROP_CO', 'TROP_SO2', 'TROP_NH3', 'TROP_HCHO', 'TROP_CH4', 'TROP_C2H6', 'TROP_C3H8', 'TROP_C2H4', 'TROP_C3H6', 'TROP_C2H5OH', 'TROP_C10H16', 'TROP_ISOP', 'TROP_CH3OH', 'TROP_isoprene', 'TROP_monoterp'
-
-#cat << EOF >> user_nl_clm 
-# doesn't work
-# hist_fincl1 = 'LAISHA', 'LAISUN', 'TLAI', 'FSH', 'FLDS', 'FSDS', 'QSOIL', 'RAINRATE', 'SNOWRATE', 'TSA', 'TSOI', 'WIND', 'ZWT', 'MEG_acetaldehyde','MEG_acetic_acid','MEG_acetone','MEG_carene_3', 'MEG_ethanol','MEG_formaldehyde','MEG_isoprene','MEG_methanol', 'MEG_pinene_a','MEG_thujene_a'
-#EOF
-
+# Simulation length
 ./xmlchange STOP_OPTION=nyears
 ./xmlchange STOP_N=1
 ./xmlchange JOB_WALLCLOCK_TIME=24:00:00
 
-#./case.build
-#./case.submit
+#./case.build --clean
+./case.setup
 
+# From Error:
+echo -e " use_init_interp = .true.">> user_nl_clm
 
-#Sara's sectional scheme:
+# Diagnostics
+cosp_diagnostics
+cam_diagnostics
 
-#fincl1 = 'SOA_SEC01','SOA_SEC02','SOA_SEC03','SOA_SEC04','SOA_SEC05',
-#'SO4_SEC01','SO4_SEC02','SO4_SEC03','SO4_SEC04','SO4_SEC05',
-#'nrSOA_SEC01','nrSOA_SEC02','nrSOA_SEC03','nrSOA_SEC04','nrSOA_SEC05',
-#'nrSO4_SEC01','nrSO4_SEC02','nrSO4_SEC03','nrSO4_SEC04','nrSO4_SEC05',
-#'cb_SOA_SEC01','cb_SOA_SEC02','cb_SOA_SEC03','cb_SOA_SEC04','cb_SOA_SEC05',
-#'cb_SO4_SEC01','cb_SO4_SEC02','cb_SO4_SEC03','cb_SO4_SEC04','cb_SO4_SEC05',
+./case.build
+./case.submit
