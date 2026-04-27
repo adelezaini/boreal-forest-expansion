8cfabd4b0401e117052e146466d42d6ea123f57c
Adele Zaini <adelez@uio.no>
2026-04-28 00:31:34 +0200

    test CTRL run

---
+++ cases-scripts/3a-NF2000norbc_tropstratchem_nudg_ctrl_f19_f19.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# Note: aerosol_cosp_diagnostics NOT NECESSARY, I forgot it
+
+### CTRL RUN
+# Nudging
+# Initial file: NF2000norbc_tropstratchem_spinup_f19_f19 (0021-01-01)
+# 15 years to start
+
+# Exit if error, undefined variable...
+set -euo pipefail
+# Load common functions for case setup
+source cases-setup.sh
+#––––––––––– SIMULATION SPECIFICS: –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+today=$(date +'%Y%m%d')
+CASENAME="NF2000norbc_tropstratchem_nudg_ctrl_f19_f19-test_$today-diagnostics"
+COMPSET=NF2000norbc_tropstratchem
+set_project_noresm_res_vars
+
+# Restart files specifics:
+REFCASE="NF2000norbc_tropstratchem_spinup_f19_f19"
+REFDATE="0021-01-01"
+
+REST_SRC="/nird/datapeak/NS9188K/adelez/BRL-FRST-XPSN_archive/${REFCASE}/rest/${REFDATE}-00000"
+REST_LOCAL="/cluster/home/$USER/restart/${REFCASE}/${REFDATE}-00000"
+# I transfer restart files because in original folder are zipped. The unzipped files are all in one place
+
+#–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+prepare_restart_files "$REST_SRC" "$REST_LOCAL"
+
+BASE_CASE_DIR="$HOME/cases/BRL_FRST_XPSN/"
+CASEROOT="$BASE_CASE_DIR/$CASENAME"
+
+remove_case_if_exists "$CASEROOT" "$BASE_CASE_DIR"
+
+cd $NORESM_ROOT/cime/scripts || exit 1
+
+./create_newcase --case $CASEROOT --compset $COMPSET --res $RES --machine betzy --run-unsupported --project $PROJECT --handle-preexisting-dirs r
+
+echo "Case $CASENAME created with compset $COMPSET and resolution $RES"
+
+cd $CASEROOT
+#–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+aerosol_cosp_diagnostics # NOT NECESSARY, I forgot it
+forcings_2000
+
+# Initial files from restart
+./xmlchange RUN_TYPE=hybrid
+./xmlchange RUN_REFCASE="$REFCASE"
+./xmlchange RUN_REFDATE="$REFDATE"
+./xmlchange RUN_REFDIR="$REST_LOCAL"
+./xmlchange GET_REFCASE=TRUE
+
+# Simulation length
+./xmlchange STOP_OPTION=nmonths,STOP_N=3 #5
+#./xmlchange RESUBMIT=2 # 5 yrs + 2 x 5 yrs = 15 yrs
+./xmlchange REST_OPTION=nyears,REST_N=1
+#./xmlchange DOUT_S_SAVE_INTERIM_RESTART_FILES=FALSE # To avoid saving restarts at the end of each run, which is not necessary for the spinup and takes a lot of space
+./xmlchange RUN_STARTDATE=2000-01-01
+
+# 3.34   simulated_years/day
+./xmlchange --subgroup case.st_archive JOB_WALLCLOCK_TIME=23:59:00
+./xmlchange --subgroup case.run        JOB_WALLCLOCK_TIME=47:59:00
+
+#–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+#./case.build --clean
+./case.setup
+#–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+setup_nudging_data 
+
+cat << EOF >> user_nl_clm
+use_init_interp = .true.
+EOF
+
+cam_diagnostics HR_BVOC
+clm_diagnostics
+
+#–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+./case.build
+./case.submit
+#–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
