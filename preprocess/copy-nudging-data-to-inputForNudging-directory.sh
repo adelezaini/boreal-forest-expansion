fd6dd58772ee5b222c702f3df4b4404a81e5f390
Adele Zaini <adelez@uio.no>
2026-04-28 00:26:47 +0200

    copy nudging h1 data from MET to inputForNudging/AZ directoy

--- preprocess/copy-nudging-data-to-inputForNudging-directory.sh
+++ preprocess/copy-nudging-data-to-inputForNudging-directory.sh
@@ -3,15 +3,16 @@
 # not tested
 
 CASENAME=NF2000norbc_tropstratchem_met_f19_f19_20260422
-TIME_RANGE=2000-2010
+TIME_RANGE=2000-2015
 FREQUENCY=6h
 
-#save-archived-case-to-boreal NF2000norbc_tropstratchem_met_f19_f19_20260422 "$CASENAME"
+#save-archived-case-to-boreal "$CASENAME"
 
 NUDGDIR=/cluster/shared/noresm/inputdata/noresm-only/inputForNudging/AZ/${CASENAME}_${FREQUENCY}_${TIME_RANGE}
-mkdir -p /cluster/shared/noresm/inputdata/noresm-only/inputForNudging/AZ/NF2000norbc_tropstratchem_met_f19_f19_20260422_6h_2000-2010
+
 mkdir -p "$NUDGDIR"
 #rsync -avP /cluster/home/adelez/BRL-FRST-XPSN_archive/$CASENAME/atm/hist/$CASENAME.cam.h1.*.nc "$NUDGDIR"/
-#ls -1v "$NUDGDIR"/*.h1.*.nc > "$NUDGDIR"/fileList.txt
+rsync -avP ~/work/archive/$CASENAME/atm/hist/$CASENAME.cam.h1.*.nc "$NUDGDIR"/
+ls -1v "$NUDGDIR"/*.h1.*.nc > "$NUDGDIR"/${CASENAME}.cam.h1_filelist.txt
 
-ls -1v /cluster/shared/noresm/inputdata/cplhist/NF2000norbc_tropstratchem_spinup_f19_f19_20260421_01-20_hist/* > /cluster/shared/noresm/inputdata/cplhist/NF2000norbc_tropstratchem_spinup_f19_f19_20260421_01-20_hist/z_fileList.txt
+#ls -1v /cluster/shared/noresm/inputdata/cplhist/NF2000norbc_tropstratchem_spinup_f19_f19_20260421_01-20_hist/* > /cluster/shared/noresm/inputdata/cplhist/NF2000norbc_tropstratchem_spinup_f19_f19_20260421_01-20_hist/z_fileList.txt
