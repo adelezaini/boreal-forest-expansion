82b6de8891ecec85abbe1fb7c0bc0bc01f7e249d
Adele Zaini <adelez@uio.no>
2026-07-05 22:26:58 +0200

    rename case scripts + README.md with case naming

---
+++ cases-scripts/23a-NF2100ssp585norbc_tropstratchem_nudg_ctrl_f19_f19.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+# I forgot cosp_diagnostics
+
+### FUT_CTRL RUN
+# Nudging
+# Initial file: NF2100ssp585norbc_tropstratchem_spinup_f19_f19-3 (0021-01-01)
+# 10 years to start
+# Land controlled to PD
+
+# Exit if error, undefined variable...
+set -euo pipefail
+# Load common functions for case setup
+source cases-setup.sh
+#––––––––––– SIMULATION SPECIFICS: –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+today=$(date +'%Y%m%d')
+CASENAME="NF2100ssp585norbc_tropstratchem_nudg_ctrl_f19_f19-$today"
+COMPSET=NF2100ssp585norbc_tropstratchem
+set_project_noresm_res_vars
+
+# Restart files specifics:
+REFCASE="NF2100ssp585norbc_tropstratchem_spinup_f19_f19-3"
+REFDATE="0021-01-01"
+
+REST_SRC="/nird/datapeak/NS9188K/adelez/BRL-FRST-XPSN_archive/${REFCASE}/rest/${REFDATE}-00000"
+REST_LOCAL="/cluster/home/$USER/restart/${REFCASE}/${REFDATE}-00000"
+# I transfer restart files because in original folder are zipped. The unzipped files are all in one place
+
+# Surface data file with modified land cover for boreal forest expansion
+SURFDATA_FILE="/cluster/shared/noresm/inputdata/lnd/clm2/surfdata_map/surfdata_1.9x2.5_78pfts_LPJGUESS_SSP585.nc"
+
+#–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+prepare_restart_files "$REST_SRC" "$REST_LOCAL"
+
+BASE_CASE_DIR="$HOME/cases/BRL_FRST_XPSN/"
+CASEROOT="$BASE_CASE_DIR/$CASENAME"
+
+remove_case_if_exists "$CASEROOT" "$BASE_CASE_DIR"
+
+cd $NORESM_ROOT/cime/scripts || exit 1
+
+./create_newcase --case $CASEROOT --compset $COMPSET --res $RES --machine betzy --run-unsupported --project $PROJECT --handle-preexisting-dirs r
+
+echo "Case $CASENAME created with compset $COMPSET and resolution $RES"
+
+cd $CASEROOT
+#–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+aerosol_cosp_diagnostics
+
+forcings_2100
+dms_forcing_2100_to_2000
+
+# Initial files from restart
+./xmlchange RUN_TYPE=hybrid
+./xmlchange RUN_REFCASE="$REFCASE"
+./xmlchange RUN_REFDATE="$REFDATE"
+./xmlchange RUN_REFDIR="$REST_LOCAL"
+./xmlchange GET_REFCASE=TRUE
+
+# Simulation length
+./xmlchange STOP_OPTION=nyears,STOP_N=5
+./xmlchange RESUBMIT=1 # 5 yrs + 1 x 5 yrs = 10 yrs
+./xmlchange REST_OPTION=nyears,REST_N=1
+#./xmlchange DOUT_S_SAVE_INTERIM_RESTART_FILES=FALSE # To avoid saving restarts at the end of each run, which is not necessary for the spinup and takes a lot of space
+./xmlchange RUN_STARTDATE=2000-01-01
+
+./xmlchange --subgroup case.st_archive JOB_WALLCLOCK_TIME=23:59:00
+./xmlchange --subgroup case.run        JOB_WALLCLOCK_TIME=47:59:00
+
+#–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+#./case.build --clean
+./case.setup
+#–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+setup_nudging_data 
+
+# Land cover change - boreal forest expansion
+# Modified idealized surfdata file
+cat << EOF >> user_nl_clm
+fsurdat = '${SURFDATA_FILE}'
+use_init_interp = .true.
+EOF
+
+cosp_diagnostics
+cam_diagnostics HR_BVOC
+clm_diagnostics
+
+./case.build
+./case.submit
---
+++ cases-scripts/README.md
@@ -0,0 +1,29 @@
+## Cases technical names:
+The numbering refers to the order of simulations.
+- first digit: [1]-PD, [2]-FUT
+- second digit: step of running (1 is required for 2)
+- letter: parallel runs (a and b can run simultaneously)
+
+- SPINUP_PD: 11-NF2000norbc_tropstratchem_spinup_f19_f19.sh
+- MET: 12a-NF2000norbc_tropstratchem_met_f19_f19.sh
+- LANDONLY_SPINUP_PD: 12b-NF2000norbc_tropstratchem_landonly_spinup_lcc_f19_f19.sh
+- CTRL_PD: 13a-NF2000norbc_tropstratchem_nudg_ctrl_f19_f19.sh
+- LCC_SPINUP_PD: 13b-NF2000norbc_tropstratchem_spinup_lcc_f19_f19.sh
+- LCC_PD: 14a-NF2000norbc_tropstratchem_nudg_lcc_f19_f19.sh
+- LCC_fBVOC_PD: 14b-NF2000norbc_tropstratchem_nudg_lcc_fBVOC_f19_f19.sh
+
+
+- SPINUP_FUT: 21-NF2100ssp585norbc_tropstratchem_spinup_f19_f19.sh
+- LANDONLY_SPINUP_FUT: 22b-NF2100ssp585norbc_tropstratchem_landonly_spinup_lcc_f19_f19.sh
+- CTRL_FUT: 23a-NF2100ssp585norbc_tropstratchem_nudg_ctrl_f19_f19.sh
+- LCC_SPINUP_FUT: 23b-NF2100ssp585norbc_tropstratchem_spinup_lcc_f19_f19.sh
+- LCC_FUT: 24a-NF2100ssp585norbc_tropstratchem_nudg_lcc_f19_f19.sh
+- LCC_FUT_fBVOC: 24b-NF2100ssp585norbc_tropstratchem_nudg_lcc_fBVOC_f19_f19.sh
+
+### Quick lcc spinups to avoid landonly+coupled spinup, used preliminary:
+- 13bx-NF2000norbc_tropstratchem_quick_spinup_lcc_f19_f19.sh
+- 23bx-NF2100ssp585norbc_tropstratchem_quick_spinup_lcc_f19_f19.sh
+
+37-NF2100ssp585norbc_tropstratchem_nudg_lcc_fFUTBVOC_f19_f19.sh
+42-NF2100ssp585norbc_tropstratchem_met_f19_f19.sh
+43-NF2100ssp585norbc_tropstratchem_fut_nudg_ctrl_f19_f19.sh
